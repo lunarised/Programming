37a9043a36746b6addc5ee29a9f45a31890c7585
lunarised <thunder.james.a@gmail.com>
2018-07-24 10:31:53 +1200

    Fixed indentation

--- Java/DinoGame/Engine.java
+++ Java/DinoGame/Engine.java
@@ -5,24 +5,20 @@ import java.io.*;
 public class Engine{
 	static Image iconImage;
 	public static void main(String[] args){
-	System.out.println("Howdy");
-	try{
-	iconImage = ImageIO.read(new File("Resources/Icon.jpg"));
-	}catch(IOException e){
-	}
-	JFrame frame = new JFrame();
+		System.out.println("Howdy");
+		try{
+			iconImage = ImageIO.read(new File("Resources/Icon.jpg"));
+		}catch(IOException e){
+		}
+		JFrame frame = new JFrame();
 
-	frame.setIconImage(iconImage);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	JPanel panel = new JPanel();
-	frame.setSize(300, 300);
-	panel.setBackground(Color.cyan);
-	frame.getContentPane().add(panel);
+		frame.setIconImage(iconImage);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		frame.setSize(300, 300);
+		panel.setBackground(Color.cyan);
+		frame.getContentPane().add(panel);
 //	frame.pack();
-       frame.setVisible(true);	
+       		frame.setVisible(true);	
 	}
-
-
-
-
 }
