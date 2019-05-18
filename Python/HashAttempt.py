9f9bea19790552cab172f0be4c2debcaa29b0704
James McKenzie <thunder.james.a@gmail.com>
2019-05-19 00:19:54 +1200

    Finished Unstable Insecure hash function

--- Python/HashAttempt.py
+++ Python/HashAttempt.py
@@ -1,7 +1,20 @@
+import random
 def hashF(inp):
-
+    n = 0
+    i = 0
+    for char in inp:
+        ab = ord(char)
+        
+        if ab == 32:
+            ab = 96
+        n += (ab-96)*((i+1)*27)
+        i += 1
+    n %= 716559
+    random.seed(n)
+    print (random.randint(1, 1000000))
     
-s1 = input("Input String 1: ")
-s2 = input("Input String 2: ")
+    
+s1 = str(input("Input String 1: "))
+s2 = str(input("Input String 2: "))
 sC = s1+s2
 hashF(sC)
