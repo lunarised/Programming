eb0938665d6678d2b71f9f31d31d8759d5d26928
lunarised <thunder.james.a@gmail.com>
2019-06-21 18:33:58 +1200

    Using Classes

--- Ruby/asgn.rb
+++ Ruby/asgn.rb
@@ -1,11 +1,27 @@
 #!/bin/ruby
-def checkResults(user, mastermind)
+class Game
+	DIFFICULTY = 10
+	def checkResults(user, mastermind)
+
+	end
+
+
+	def userInput(prompt: "")	#Takes an optional prompt which will be stored in prompt. In this case
+					#I will use as a prompt
+		puts prompt if prompt 	#Prints out the Prompt
+		gets			#Reads User Input
+	end				#Returns the result of gets
+	guesses = 0
+	until guesses == DIFFICULTY
+	
+		userInput(prompt: "What is my name")
+		guesses++
+	end
 
 end
-def userInput(*args) 	#Takes an optional argument which will be stored in args. In this case
-			#I will use as a prompt
-puts args 		#Prints out the Prompt stored in args
-gets			#Reads User Input
-end			#Returns the result of gets
+class StringCheck
 
-userInput("What is my name")
+
+end
+
+Game
