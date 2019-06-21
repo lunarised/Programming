5815626362b73cc389abcdda0193ce4ba9634ac9
lunarised <thunder.james.a@gmail.com>
2019-06-21 19:08:50 +1200

    Using Modules

--- Ruby/asgn.rb
+++ Ruby/asgn.rb
@@ -1,17 +1,17 @@
 #!/bin/ruby
-class Game
+module Game
 	DIFFICULTY = 10
 	def checkResults(user, mastermind)
 
 	end
 
 
-	def self.userInput(prompt: "")	#Takes an optional prompt which will be stored in prompt. In this case
+	def userInput(prompt: "")	#Takes an optional prompt which will be stored in prompt. In this case
 					#I will use as a prompt
 		puts prompt if prompt 	#Prints out the Prompt
 		gets			#Reads User Input
 	end				#Returns the result of gets
-	def self.run
+	def run
 		guesses = 0
 		until guesses == DIFFICULTY	
 			userInput(prompt: "What is my name")
@@ -19,7 +19,7 @@ class Game
 		end
 	end
 end
-class StringValidator
+module StringValidator
 	def stringLenCheck(instring)
 		instring.length==5
 	end
