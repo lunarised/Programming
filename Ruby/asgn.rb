239b5060a7c26445c5fae81171400d60e3016c55
lunarised <thunder.james.a@gmail.com>
2019-06-21 20:02:35 +1200

    Edited more functionality

--- Ruby/asgn.rb
+++ Ruby/asgn.rb
@@ -1,5 +1,6 @@
 #!/bin/ruby
 module Game
+	module_function
 	DIFFICULTY = 10
 	def checkResults(user, mastermind)
 
@@ -9,32 +10,40 @@ module Game
 	def userInput(prompt: "")	#Takes an optional prompt which will be stored in prompt. In this case
 					#I will use as a prompt
 		puts prompt if prompt 	#Prints out the Prompt
-		gets			#Reads User Input
+		gets.chomp			#Reads User Input
 	end				#Returns the result of gets
 	def run
 		guesses = 0
 		until guesses == DIFFICULTY	
-			userInput(prompt: "What is my name")
+			ui = userInput(prompt: "What is my name")
+			if StringValidator.run(ui)
 			guesses += 1
+			
+			else
+				puts "invalid input"
+			end
 		end
 	end
 end
 module StringValidator
+	module_function
 	def stringLenCheck(instring)
 		instring.length==5
 	end
 	def stringContentCheck(instring)
-		succ = true
-		characters = instring.split('')
-		chars.each{ |c|
-			unless letter?(c)	
-				succ = false;
-			end
-		}
-	succ	
+		instring =~ /^[A-Za-z]+$/	
 	end
-	def letter?(letter)
-  		letter =~ /[A-Za-z]/
+	def duplicateChecker(instring)
+		dsucc = true
+		dsucc = false if instring.each_char.find{|c| instring.count(c)>1}
+	dsucc
+	end
+	def run(instring)
+		succ = false		
+		if stringLenCheck(instring) and stringContentCheck(instring) and duplicateChecker(instring)
+			succ =  true
+		end
+	succ
 	end
 end
 
