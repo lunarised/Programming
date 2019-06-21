e1887a899c02921b8f5e06433f0d8d3891001438
lunarised <thunder.james.a@gmail.com>
2019-06-21 18:18:55 +1200

    Commented

---
+++ Ruby/asgn.rb
@@ -0,0 +1,11 @@
+#!/bin/ruby
+def checkResults(user, mastermind)
+
+end
+def userInput(*args) 	#Takes an optional argument which will be stored in args. In this case
+			#I will use as a prompt
+puts args 		#Prints out the Prompt stored in args
+gets			#Reads User Input
+end			#Returns the result of gets
+
+userInput("What is my name")
