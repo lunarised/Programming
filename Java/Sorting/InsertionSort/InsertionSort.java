9f8d19095a7e73390931753231ecb3b0288c29ce
lunarised <j@p7.co.nz>
2019-09-25 23:15:24 +1200

    Insertion Sort Fixed (After a year)

--- Java/Sorting/InsertionSort/InsertionSort.java
+++ Java/Sorting/InsertionSort/InsertionSort.java
@@ -3,15 +3,23 @@ public class InsertionSort{
 
 	public static void main(String[] args){
 	System.out.println("Hello World");
-	int[] unsorted = new int[]{5,4,3,2,1};
-	int[] sorted = new int[5];
+	int[] unsorted = new int[]{22,6969,420,2,1};
+	int hold;
 	for(int i = 1; i<unsorted.length; i++){
+		hold = unsorted[i];
 		for(int j = i; j>0; j--) {
-			System.out.print(sorted[j]);
-
+			if (hold<unsorted[j-1]){
+				hold = unsorted[j-1];
+				unsorted[j-1] = unsorted[j];
+				unsorted[j] = hold;
+				hold = unsorted[j-1];
+			}
 		}
 	}
-
+	for (int val: unsorted){
+		System.out.print(val + " ");
+	}
+	System.out.print("\n");
 	}
 
 	}
