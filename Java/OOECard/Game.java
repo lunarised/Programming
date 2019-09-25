13dede132b947c7b4e06d6b6adbf020f57e8c587
lunarised <j@p7.co.nz>
2019-09-25 21:28:32 +1200

    Added Player Selection

--- Java/OOECard/Game.java
+++ Java/OOECard/Game.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
 class Game{
     private static final int PLAYERS = 2;
     private static final int HANDSIZE = 5;
 
     private static Hand emperorsHand;
     private static Hand[] slaveHands;
+    private static char playerSide;
+    
+    private static Scanner sc;
 
     public static void main(String[] args){
         initState();
+        String inp = sc.nextLine();
+        playerSide = inp.charAt(0);
+        System.out.println(playerSide);
+        
+        
         System.out.println(slaveHands[0].getCardAt(4));
     }
     public static void initState(){
@@ -15,5 +24,6 @@ class Game{
         for (int i=0; i<PLAYERS -1; i++){
             slaveHands[i] = new Hand('S', HANDSIZE);
         }
+        sc = new Scanner(System.in);
     }
 }
